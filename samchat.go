e6997388086c83c2b8179bdeb08e8cd4a8029aab
prozz <prozzz@gmail.com>
2015-03-14 13:46:15 +0100

    renames

--- samchat.go
+++ samchat.go
@@ -11,15 +11,16 @@ func main() {
 	flag.Parse()
 
 	s := mobster.NewServer()
+	s.Debug = true
 
-	s.OnConnect = func(ctx *mobster.Ops, name, room string) {
-		ctx.SendToRoom(room, fmt.Sprintf("%s joins", name))
+	s.OnConnect = func(ops *mobster.Ops, user, room string) {
+		ops.SendToRoom(room, fmt.Sprintf("%s joins", user))
 	}
-	s.OnDisconnect = func(ctx *mobster.Ops, name, room string) {
-		ctx.SendToRoom(room, fmt.Sprintf("%s leaves", name))
+	s.OnDisconnect = func(ops *mobster.Ops, user, room string) {
+		ops.SendToRoom(room, fmt.Sprintf("%s leaves", user))
 	}
-	s.OnMessage = func(ctx *mobster.Ops, name, room, message string) {
-		ctx.SendToRoom(room, fmt.Sprintf("%s: %s", name, message))
+	s.OnMessage = func(ops *mobster.Ops, user, room, message string) {
+		ops.SendToRoom(room, fmt.Sprintf("%s: %s", user, message))
 	}
 
 	s.StartServerAndWait(*port)
